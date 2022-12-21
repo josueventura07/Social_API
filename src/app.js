26a1ae7710762cf8206f38dd593aa2c542d74d02
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2022-12-21 14:26:19 -0600

    solve merge problem

--- src/app.js
+++ src/app.js
@@ -9,10 +9,7 @@ const initModels = require('./models/initModels')
 const userRouter = require('./users/users.router')
 const authRouter = require('./auth/auth.router')
 const postRouter = require('./posts/posts.router')
-<<<<<<< HEAD
-=======
 const followRouter = require('./follows/follows.router')
->>>>>>> development
 
 //? Initial Configs
 
@@ -45,10 +42,7 @@ app.get('/', (req, res) => {
     })
 })
 
-<<<<<<< HEAD
-=======
 app.use('/api/v1', followRouter)
->>>>>>> development
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/posts', postRouter)
